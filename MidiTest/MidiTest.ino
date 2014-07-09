e4c3f31bb44648ad5d75b56a31e7874de9017229
Jamie Bliss <astronouth7303@gmail.com>
2014-07-09 19:14:50 -0400

    Add two more testing sketches

---
+++ MidiTest/MidiTest.ino
@@ -0,0 +1,59 @@
+#include <Wire.h>
+#include "htk.h"
+
+#define LED LED_BUILTIN
+#define CHANNEL 1
+#define VELOCITY 99
+
+HT16K33 keymagic(0x70);
+
+HT16K33::keystruct last = {0,0,0};
+
+void setup() 
+{
+  pinMode(LED, OUTPUT);
+  Wire.begin(); // Must be done before any HT stuff works
+
+  digitalWrite(LED, HIGH);
+  keymagic.setup(true); // Turn on oscillator
+  if (keymagic.err) Serial.println(keymagic.err);
+  keymagic.display_setup(false); // Turn off display
+  if (keymagic.err) Serial.println(keymagic.err);
+  keymagic.rowint(false); // Disable INT
+  if (keymagic.err) Serial.println(keymagic.err);
+  digitalWrite(LED, LOW);
+}
+
+void loop() 
+{
+  uint8_t intval = keymagic.interrupt();
+  if (keymagic.keyready()) 
+  {
+    HT16K33::keystruct keys = keymagic.keydata();
+    digitalWrite(LED, keys.KS0 || keys.KS1 || keys.KS2);
+    for (uint8_t i = 0; i < 3; i++) 
+    {
+      for (uint8_t m = 0; m < 13; m++) 
+      {
+        size_t b = 1 << m;
+        if ((keys.keys[i] & b) != (last.keys[i] & b)) 
+        {
+          unsigned int note = i * 13 + m + 1;
+          if (keys.keys[i] & b) 
+          {
+            usbMIDI.sendNoteOn(note, VELOCITY, CHANNEL);
+          }
+          else
+          {
+            usbMIDI.sendNoteOff(note, 0, CHANNEL);
+          }
+        }
+      }
+    }
+    last = keys;
+  }
+  else
+  {
+    while (usbMIDI.read()) {}
+  }
+}
---
+++ MidiTest/htk.cpp
@@ -0,0 +1,140 @@
+#include "htk.h"
+#include <Wire.h>
+
+HT16K33::HT16K33(uint8_t addr)
+{
+  _addr = addr;
+  err = HT16K33::NOERROR;
+  _nextkey = 0;
+}
+
+void HT16K33::begin()
+{
+    setup(true); // Turn on oscillator
+}
+
+void HT16K33::command(htaddr cmd)
+{
+  Wire.beginTransmission(_addr);
+  Wire.write(cmd);
+  err = (HT16K33::Error)Wire.endTransmission();
+}
+
+// Stream writing
+void HT16K33::beginWrite(HT16K33::htaddr addr)
+{
+  Wire.beginTransmission(_addr);
+  Wire.write(addr);
+}
+
+void HT16K33::write(uint8_t data)
+{
+  Wire.write(data);
+}
+
+void HT16K33::write(const uint8_t* data, size_t len)
+{
+  Wire.write(data, len);
+}
+
+void HT16K33::endWrite()
+{
+  err = (HT16K33::Error)Wire.endTransmission();
+}
+
+// One-off writing
+void HT16K33::write(HT16K33::htaddr addr, uint8_t data)
+{
+  beginWrite(addr);
+  write(data);
+  endWrite();
+}
+
+void HT16K33::write(HT16K33::htaddr addr, const uint8_t* data, size_t len)
+{
+  beginWrite(addr);
+  write(data, len);
+  endWrite();
+}
+
+// Stream reading
+void HT16K33::beginRead(HT16K33::htaddr addr, uint8_t len)
+{
+  command(addr);
+  Wire.requestFrom(_addr, len);
+}
+
+bool HT16K33::available()
+{
+  return Wire.available() ? true : false;
+}
+
+uint8_t HT16K33::read()
+{
+  return Wire.read();
+}
+
+// One-off reading
+uint8_t HT16K33::read(HT16K33::htaddr addr)
+{
+  beginRead(addr, 1);
+  return read();
+}
+
+void HT16K33::setup(bool osc)
+{
+  command(0b00100000 | (osc?1:0));
+}
+
+void HT16K33::rowint(bool rowint, bool act)
+{
+  uint8_t cmd = 0b10100000;
+  if (rowint) {
+    cmd |= 0b1;
+  }
+  if (act) {
+    cmd |= 0b10;
+  }
+  command(cmd);
+}
+
+void HT16K33::display_setup(bool on, Blink blink)
+{
+  uint8_t cmd = 0b10000000;
+  if (on) {
+    cmd |= 0b1;
+  }
+  cmd |= blink;
+  command(cmd);
+}
+
+//void HT16K33::display(...)
+
+void HT16K33::dimming(uint8_t level)
+{
+  command(0b11100000 | ((level - 1) & 0b1111));
+}
+
+HT16K33::keystruct HT16K33::keydata()
+{
+  HT16K33::keystruct rv = {{0,0,0}};
+  _nextkey = millis() + HT16K33_KEYTIME;
+  beginRead(0x40, 6);
+  uint8_t low = read();
+  uint16_t high = read();
+  rv.keys[0] = (high << 8) | low;
+  low = read();
+  high = read();
+  rv.keys[1] = (high << 8) | low;
+  low = read();
+  high = read();
+  rv.keys[2] = (high << 8) | low;
+  endRead();
+  return rv;
+}
+
+bool HT16K33::interrupt()
+{
+  return read(0x60);
+}
+
